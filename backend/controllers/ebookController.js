8300f742b6504bf2a1bed8ffa8208445a2253ea3
amrit-kerketta-au5 <amritkerketta90@gmail.com>
2020-06-28 10:39:12 +0530

    Ebook

--- backend/controllers/ebookController.js
+++ backend/controllers/ebookController.js
@@ -1,14 +1,38 @@
 const ebook = require('../models/ebookModel')
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-exports.getebook = async (req, res) => {
-  res.json({ msg: "All ebook" });
-};
+exports.getebook = catchAsync(async (req, res, next) => {
+  const { name } = req.body;
+  console.log(req.body);
+
+  if (!name) {
+    return next(new AppError(`Provide a valid name`, 400));
+  }
+
+  const book = await ebook.findOne({ name });
+
+  if (!book) {
+    return next(new AppError(`No Ebook with name: ${name}`, 400));
+  }
+  res.json({
+    status: true,
+    data: book,
+  });
+});
 
 exports.saveebook = catchAsync(async (req, res, next) => {
   const { description } = req.body;
   console.log(req.file)
-  let file = await req.file.path
+  let file = ''
+  let filename = ''
+  if (req.file.path) {
+
+    file = await req.file.path
+    filename = await req.file.filename
+  }
+  else {
+    res.json('File not found')
+  }
   //validate the data
   if (!description) {
     return next(new AppError("Please provide Description", 400));
@@ -17,6 +41,7 @@ exports.saveebook = catchAsync(async (req, res, next) => {
     return next(new AppError("Please provide file", 400));
   }
 
+
   //upload the file to the server
 
 
@@ -24,7 +49,8 @@ exports.saveebook = catchAsync(async (req, res, next) => {
   //save the file path of the server to the mongodb
   const ebookobj = new ebook({
     description,
-    file
+    filepath: file,
+    name: filename
   })
   await ebook.findOne({ file: file }).then(result => {
     if (!result) {
--- backend/models/ebookModel.js
+++ backend/models/ebookModel.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
 const ebookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   description: {
     type: String,
     required: true,
   },
-  file: {
+  filepath: {
     type: String,
     required: true,
     unique: true,
