12ae5a0e916e9608190be9c913ebd58180fcb134
amrit-kerketta-au5 <amritkerketta90@gmail.com>
2020-07-10 06:14:05 +0530

    Updates

--- frontend/src/components/courseDashboard/StudentEbookPanel.js
+++ frontend/src/components/courseDashboard/StudentEbookPanel.js
@@ -7,15 +7,17 @@ function Ebook(props) {
     const [description, setDescription] = useState([])
     const [link, setlink] = useState([])
 
-    useEffect(() => {
+    useEffect(async () => {
         const url = 'http://localhost:4000/api/getebook/'
         const id = props.courseID
         const finalurl = url + id
-        axios.get(finalurl).then(response => response.data).then(data => {
-            //console.log(data.ebookdata[0].ebooks.length);
+        await axios.get(finalurl).then(response => response.data).then(data => {
+            // console.log(data.ebookdata[0].ebooks[1].filepath);
 
             if (data.ebookdata[0].ebooks.length > 0) {
                 data.ebookdata[0].ebooks.forEach(d => {
+
+
                     setlink(link => [...link, d.filepath])
                     setDescription(description => [...description, d.description])
                 })
@@ -24,21 +26,23 @@ function Ebook(props) {
         })
 
     }, [])
+
     return (
+
         <div>
             <div className="container" style={{ marginTop: '-18px' }}>
                 <div className="row">
                     <div className="col-md-1"></div>
                     <div className="col-md-10">
                         {
-                            link.map((link, index) => {
+                            description.map((d, i) => {
                                 return < Paper
-                                    elevation={3} key={index}
+                                    elevation={3} key={i}
                                 >
-
-                                    <Typography variant='h7' style={{ marginLeft: '20px' }}>
-                                        Description:{description[index]}<br />
-                                    Link:<a href={link[index]}>Download Link</a>
+                                    {console.log("Link:", link[i], "Description:", d)}
+                                    <Typography variant='h6' style={{ marginLeft: '20px' }}>
+                                        Description:{d}<br />
+                                    Link:<a href={link[i]}>Download Link</a>
                                     </Typography>
                                 </Paper>
                             })
